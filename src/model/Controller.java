bea189be18fcb680d06106ca988bc78cda8344e8
Juan Sebastian Benitez <jsbc450@gmail.com>
2026-04-30 08:49:20 -0500

    entrega seguimiento

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,5 +1,60 @@
 package model;
 
+
 public class Controller {
+
+    private int floors = 12;
+    private int devicesPerFloor = 4;
+    private Device [][]  deviceMatrix;
+
+    private int totalDevicesah;
+
+    public Controller() {
+        deviceMatrix = new Device[floors][devicesPerFloor];
+    }
+
+    public boolean addDevice(String serial, double consumption, String description, int floor) {
+        
+        if (searchDevice(serial) != null) {
+            return false;
+        }
+        for (int j = 0; j < deviceMatrix[floor].length; j ++) {
+            if(deviceMatrix[floor][j] == null) {
+                deviceMatrix[floor][j] = new Device(serial, consumption, description);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean registerEventToDevice(String serial, double duration) {
+
+    }
+
+    public double calculateTotalConsumption(String serial) {
+        Device foundDevice = searchDevice(serial);
+        if (foundDevice != null) {
+            double totalHours = foundDevice.calculateTotalHours();
+
+            double totalConsumption = totalHours * foundDevice.getConsumption();
+            return totalConsumption;
+        }
+    }
+
+    public String getHighConsumptionReport() {
+
+    }
+
+    private Device searchDevice(String serial) {
+    for (int i = 0; i < floors; i++) {
+        for (int j = 0; j < devicesPerFloor; j++) {
+            
+            if (deviceMatrix[i][j] != null && deviceMatrix[i][j].getSerial().equals(serial)) {
+                return deviceMatrix[i][j];
+            }
+        }
+    }
+    return null; 
+}
 
 }
--- src/model/Device.java
+++ src/model/Device.java
@@ -1,5 +1,30 @@
 package model;
-
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 public class Device {
+private String serial;
+private double consumption;
+private String description;
+
+public Device(String serial, double consumption, String description) {
+    this.serial = serial;
+    this.consumption = consumption;
+    this.description = description;
+}
+
+
+public void addEvent(LocalDateTime timestamp, double duration) {
+        Event nEvent = new Event(timestamp, duration);
+        this.events.add(nEvent);
+}
+
+public double calculateTotalHours () {
+    double totalHours = 0;
+    for (Event event : events) {
+        totalHours += event.getDuration(); 
+    }
+    return totalHours;
+}
+
 
 }
